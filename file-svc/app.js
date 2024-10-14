56d37e29ad6780e1b34cb1c64c9360b948c656ea
Ender Sotelo <endersotelo@endersoteloM1.lan>
2024-10-13 21:27:41 -0700

    s3 upload in file-svc, file-svc recieves file and upload to s3 then deletes file

--- file-svc/app.js
+++ file-svc/app.js
@@ -1,17 +1,34 @@
+const fs = require('fs')
 const express = require("express")
 const app = express()
 app.use(express.json());
 
+const AWS = require("aws-sdk")
+require('dotenv').config();
+console.log(process.env.S3_BUCKET)
+AWS.config.update({ region: process.env.S3_REGION })
+const s3 = new AWS.S3({ apiVersion: '2006-03-01' })
+
+var uploadParams = { Bucket: process.env.S3_BUCKET, Key: "", Body: "" };
+
 app.post('/api/upload', (req, res) => {
     console.log(req.body.filePath)
-    uploadParams.Key = req.file.originalname;
-    uploadParams.Body = req.file.buffer;
-    s3.upload(uploadParams, (err,data) => {
-        if(err) {
-            console.log(err);
-            return res.status(500).send('Error uploading file');
-        }
-        res.send('Succesfully uploaded')
+    fs.readFile(req.body.filePath, (err, data) => {
+        // Display the file content
+        console.log(data);
+        uploadParams.Key = req.body.fileName;
+        uploadParams.Body = data;
+        s3.upload(uploadParams, (err,data) => {
+            if(err) {
+                console.log(err);
+                return res.status(500).send('Error uploading file');
+            }
+            console.log('success')
+            fs.unlink(req.body.filePath, (err) => {
+                if (err) throw err;
+                console.log(req.body.filePath + ' was deleted');
+            });
+        });
     });
 });
 
